82f4d70e37402f185bd9fe5dfb1218478ac72052
jpfstn <joaofmarques.dev@gmail.com>
2023-04-26 21:45:43 -0300

    Inicializando repositorio

---
+++ src/app/cars/page.js
@@ -0,0 +1,9 @@
+const Componente = () => {
+    return <div className="bg-white text-black p-4 text-lg">
+        Primeiro teste
+    </div>
+}
+
+export default function Home() {
+    return <Componente></Componente>
+}
