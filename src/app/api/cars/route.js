077c6aabb0e9a284e5467b931bafbd3de588c3a7
Luiz de Oliveira <luizcarlos1405@gmail.com>
2023-05-13 19:27:57 -0400

    Install daisyui and get formData in the backend

--- src/app/api/cars/route.js
+++ src/app/api/cars/route.js
@@ -1,3 +1,8 @@
-export async function GET(request) {
-  return new Response("Cars!");
+export async function POST(req, res) {
+  // Get data submitted in request's body.
+  const formData = await req.formData();
+  const [name] = formData.getAll("name");
+  console.log(`POST name:`, name); // __AUTO_GENERATED_PRINT_VAR__
+  const [brand] = formData.getAll("brand");
+  console.log(`POST brand:`, brand); // __AUTO_GENERATED_PRINT_VAR__
 }
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,12 @@ export default async function Home() {
   const cars = await carsCollection.find({}).toArray();
   console.log(`Home cars:`, cars); // __AUTO_GENERATED_PRINT_VAR__
   return (
-    <main className="">
+    <main className="w-full h-full bg-base-300">
       <div>
-        <form>
-          <input />
-          <input />
+        <form className="flex flex-col space-y-3" onSubmit={console.log}>
+          <input name="name" className="input" />
+          <input name="brand" className="input" />
+          <button type="submit">Enviar</button>
         </form>
       </div>
     </main>
